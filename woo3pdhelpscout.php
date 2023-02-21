beb42e1e101794487afdee47c91a8fabfb0ac4b3
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2023-02-21 14:07:15 -0700

    update plugin header to require php 8.

--- woo3pdhelpscout.php
+++ woo3pdhelpscout.php
@@ -1,29 +1,32 @@
 <?php
-/*
-Plugin Name: Woo3pd Helpscout Integration
-Plugin URI: http://www.kathyisawesome.com/
-Description: Parse WooCommerce.com support emails into HelpScout conversations
-Version: 1.0.0-alpha.1
-Author: Kathy Darling
-Author URI: http://www.kathyisawesome.com
-License: GPL3
-Text Domain: woo3pdhelpscout
-
-Copyright 2020  Kathy Darling  (email: kathy@kathyisawesome.com)
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License, version 2, as
-published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+/**
+ * Plugin Name: Woo3pd Helpscout Integration
+ * Plugin URI: http://www.kathyisawesome.com/
+ * Description: Parse WooCommerce.com support emails into HelpScout conversations
+ * Version: 1.0.0-alpha.1
+ * Author: Kathy Darling
+ * Author URI: http://www.kathyisawesome.com
+ * License: GPL3
+ * Text Domain: woo3pdhelpscout
+ *
+ * Requires at least: 6.0.0
+ * Requires PHP: 8.0
+ * 
+ * Copyright 2020  Kathy Darling  (email: kathy@kathyisawesome.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
